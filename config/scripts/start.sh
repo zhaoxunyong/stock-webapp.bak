e794aebdcc2179faa919395db70e5978fd9c9894
赵训勇 <dave.zhao@aeasycredit.com>
2017-11-08 14:10:11 +0800

    Add files.

--- config/scripts/start.sh
+++ config/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 . /etc/profile
-java -Xmx128m -Xms128m -Djava.security.egd=file:/dev/./urandom -jar employee-server-1.0.0-SNAPSHOT.jar --spring.profiles.active=dev > /var/log/aeasycredit/hkcash/employee-server-full.log 2>&1
+java -Xmx128m -Xms128m -Djava.security.egd=file:/dev/./urandom -jar stock-webapp-1.0.0-SNAPSHOT.jar --spring.profiles.active=dev > stock-webapp.log 2>&1
--- src/test/java/com/stock/fetch/FetchServiceTest.java
+++ src/test/java/com/stock/fetch/FetchServiceTest.java
@@ -1,31 +1,13 @@
-/*
- * 描述： <描述>
- * 修改人： Dave.zhao
- * 修改时间： Jul 19, 2017
- * 项目： aeasycredit-commons-cache
- */
 package com.stock.fetch;
-
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.stock.StockApplication;
 import org.stock.fetch.service.FetchService;
 
-/**
- * <功能简述><br>
- * <功能详细描述>
- * 
- * @author Dave.zhao
- * @version [版本号, Jul 19, 2017]
- * @see [相关类/方法]
- * @since [产品/模块版本]
- */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=StockApplication.class)
 public class FetchServiceTest {
