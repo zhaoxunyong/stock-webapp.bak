4a930f1779716d1b4e0fb8cae9b7bfdf7fe06e1d
zhaoxy <zhaoxy@zhaoxy-PC>
2018-03-17 00:02:40 +0800

    Mod add files.

--- backend/src/main/java/org/stock/StockApplication.java
+++ backend/src/main/java/org/stock/StockApplication.java
@@ -2,8 +2,10 @@ package org.stock;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockApplication {
     
     public static void main(String[] args) {
