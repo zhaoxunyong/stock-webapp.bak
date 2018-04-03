d88aade8ed7ee7775a8e2c9e63520b86d3ff36d2
zhaoxunyong <zhaoxunyong@qq.com>
2018-04-03 16:41:24 +0800

    Del delete unuse files.

---
+++ frontend/init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cnpm install --save
+cnpm install bootstrap-vue --save
+cnpm install node-sass --save
+cnpm install axios --save
+cnpm install vue-confirm --save
+cnpm install open-iconic --save
+cnpm install sortablejs --save
+cnpm install stock-auto-complete --save
+#cnpm install stylus stylus-loader style-loader --save
+cnpm install --save vue-message
+cnpm install http-server -g
