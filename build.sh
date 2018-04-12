0418535c9c009b9665f2dc409b4adbc35f3a474d
zhaoxunyong <zhaoxunyong@qq.com>
2018-04-12 23:53:59 +0800

    Add files.

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 cd frontend
 mvn clean install -Dmaven.test.skip=true
-if [[ $? == 0 ]]; then
-  echo "frontend build error."
-  exit -1
-fi
 cd ../backend
 mvn clean install -Dmaven.test.skip=true
