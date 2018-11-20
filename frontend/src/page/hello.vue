55858250bb62f4ffe79055a3a7544f79da70b1fc
dave.zhao <dave.zhao@zerofinance.cn>
2018-11-20 14:25:24 +0800

    Mod 更新文档。

---
+++ frontend/src/page/hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-bind:class="bgYellow">
+    {{heading}}
+  </div>
+</template>
+<script> 
+ 
+export default {
+  data () {
+    return {
+      heading: 'Hello World',
+      bgYellow: 'bgYellow'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bgYellow{
+  background-color: yellow;
+}
+</style>
