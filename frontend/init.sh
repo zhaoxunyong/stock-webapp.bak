e64d03a5873f66bde525a258f5eabcaab666930e
zhaoxunyong <zhaoxunyong@qq.com>
2018-11-28 18:32:41 +0800

    Mod 修改脚本内容

--- frontend/init.sh
+++ frontend/init.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
-#cnpm cache clean --force
-cnpm install --save
+#npm cache clean --force
+npm install --save --registry=https://registry.npm.taobao.org
 #2.0.0-rc.6
-cnpm install bootstrap-vue --save
-cnpm install node-sass --save
-cnpm install axios --save
-cnpm install vue-confirm --save
-cnpm install open-iconic --save
-cnpm install sortablejs --save
-cnpm install stock-auto-complete --save
-cnpm install vue-message --save
-#cnpm install mathjs --save
-#cnpm install decimal.js --save
+npm install bootstrap-vue --save --registry=https://registry.npm.taobao.org
+npm install node-sass --save --registry=https://registry.npm.taobao.org
+npm install axios --save --registry=https://registry.npm.taobao.org
+npm install vue-confirm --save --registry=https://registry.npm.taobao.org
+npm install open-iconic --save --registry=https://registry.npm.taobao.org
+npm install sortablejs --save --registry=https://registry.npm.taobao.org
+npm install stock-auto-complete --save --registry=https://registry.npm.taobao.org
+npm install vue-message --save --registry=https://registry.npm.taobao.org
+#npm install mathjs --save
+#npm install decimal.js --save
 # resloved:Vue BrowserslistError: Unknown browser query `dead`
 #https://github.com/browserslist/browserslist/issues/276
 #Downgraded bootstrap to 4.1.0 fixes the issue for me
-#cnpm uninstall bootstrap --save
-#cnpm install bootstrap@4.1.0 --save
+#npm uninstall bootstrap --save
+#npm install bootstrap@4.1.0 --save
 
 npm install echarts --registry=https://registry.npm.taobao.org
-#cnpm install vue-echarts --save
+#npm install vue-echarts --save
 
-cnpm install http-server -g
+npm install http-server -g --registry=https://registry.npm.taobao.org
 
-#cnpm install stylus stylus-loader style-loader --save-dev
-#cnpm install --save bollinger-bands
+#npm install stylus stylus-loader style-loader --save-dev
+#npm install --save bollinger-bands
 
-cnpm install --save technicalindicators@1.1.13
+npm install --save technicalindicators@1.1.13 --registry=https://registry.npm.taobao.org
 
 #npm uninstall node-sass
 #npm i node-sass --sass_binary_site=https://npm.taobao.org/mirrors/node-sass/
