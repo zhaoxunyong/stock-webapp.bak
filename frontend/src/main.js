e1031910344c92c1e4954507cda0ba58acd4d19f
zhaoxunyong <zhaoxunyong@163.com>
2018-11-28 23:12:00 +0800

    echart优化加载

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -55,7 +55,17 @@ import axios from 'axios'
 // import ECharts from 'vue-echarts'
 // Vue.component('chart', ECharts)
 
-import echarts from 'echarts'
+// import echarts from 'echarts'
+// 引入 ECharts 主模块
+import echarts from 'echarts/lib/echarts'
+// 引入柱状图
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/chart/candlestick'
+import 'echarts/lib/chart/line'
+// 引入提示框和标题组件
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
+
 Vue.prototype.$echarts = echarts
 
 new Vue({
